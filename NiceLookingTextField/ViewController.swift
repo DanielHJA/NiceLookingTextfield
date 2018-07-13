8095a6f2d0801b5309f6590f48656aa73d54f5f1
Daniel <daniel.hjartstrom@gmail.com>
2018-07-13 22:17:51 +0100

    Minor fixes and stackview test

--- NiceLookingTextField/ViewController.swift
+++ NiceLookingTextField/ViewController.swift
@@ -20,27 +20,61 @@ extension DispatchQueueProtocol {
 class ViewController: UIViewController, DispatchQueueProtocol {
 
     private lazy var testView: TextfieldView = {
-        let temp = TextfieldView(frame: CGRect(x: 0, y: 50.0, width: view.frame.width, height: 80.0))
+        let temp = TextfieldView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 80.0))
         temp.setTitle = "Username"
         temp.setBackgroundColor = UIColor.blue
         temp.setTextfieldColor = UIColor.white
         temp.setTitleColor = UIColor.white
         temp.setBorderColor = UIColor.white
         temp.shouldAnimate = true
-        view.addSubview(temp)
+        /*  view.addSubview(temp)
+         temp.translatesAutoresizingMaskIntoConstraints = false
+         temp.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+         temp.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+         temp.topAnchor.constraint(equalTo: view.topAnchor, constant: 50.0).isActive = true
+         temp.heightAnchor.constraint(equalToConstant: 80.0).isActive = true*/
+        return temp
+    }()
+    
+    private lazy var testView2: TextfieldView = {
+        let temp = TextfieldView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 80.0))
+        temp.setTitle = "Password"
+        temp.setBackgroundColor = UIColor.blue
+        temp.setTextfieldColor = UIColor.white
+        temp.setTitleColor = UIColor.white
+        temp.setBorderColor = UIColor.white
+        temp.shouldAnimate = true
+      /*  view.addSubview(temp)
         temp.translatesAutoresizingMaskIntoConstraints = false
         temp.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         temp.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         temp.topAnchor.constraint(equalTo: view.topAnchor, constant: 50.0).isActive = true
-        temp.heightAnchor.constraint(equalToConstant: 80.0).isActive = true
+        temp.heightAnchor.constraint(equalToConstant: 80.0).isActive = true*/
+        return temp
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let temp = UIStackView()
+        temp.alignment = .fill
+        temp.axis = .vertical
+        temp.distribution = .fillEqually
+        view.addSubview(temp)
+        temp.translatesAutoresizingMaskIntoConstraints = false
+        temp.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        temp.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        temp.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        temp.heightAnchor.constraint(equalToConstant: 160.0).isActive = true
         return temp
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        testView.isHidden = false
+
+        stackView.isHidden = false
+        stackView.addArrangedSubview(testView)
+        stackView.addArrangedSubview(testView2)
         
-        delay(time: 3.0) { [weak self] in
+        /*delay(time: 3.0) { [weak self] in
             self?.testView.isTextValid = true
             self?.delay(time: 3.0) { [weak self] in
                 self?.testView.isTextValid = false
@@ -49,11 +83,14 @@ class ViewController: UIViewController, DispatchQueueProtocol {
                     self?.testView.isTextValid = true
                 }
             }
-        }
+        }*/
         
-        
-        
-        
+        delay(time: 3.0) { [unowned self] in
+            self.testView2.isTextValid = false
+                self.delay(time: 3.0) { [unowned self] in
+                    self.testView.isTextValid = true
+                }
+            }
     }
 
     override func didReceiveMemoryWarning() {
